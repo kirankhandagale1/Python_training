0135b1f6ffc1083accffe56f4794d0aef8b40148
kirankhandagale1 <kirankhandagale74@gmail.com>
2019-05-06 18:25:14 +0530

    second_prg

---
+++ Excercise1/p1_1.py
@@ -0,0 +1,6 @@
+# Check Python is installed or not and check the Python version
+
+import sys
+version=sys.version_info
+print(version)
+                            #cmd for linux..(python --version)
